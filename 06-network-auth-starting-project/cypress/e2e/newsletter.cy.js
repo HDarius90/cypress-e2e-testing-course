6c7034afcd15bdebf113ea924bb83c985d4bbdca
Hadjati Darius <darius.hadjati@gmail.com>
2024-06-06 19:30:43 +0200

    Understanding & Using Interceptors

--- 06-network-auth-starting-project/cypress/e2e/newsletter.cy.js
+++ 06-network-auth-starting-project/cypress/e2e/newsletter.cy.js
@@ -3,9 +3,11 @@ describe("Newsletter", () => {
     cy.task("seedDatabase");
   });
   it("should display a success message", () => {
+    cy.intercept("POST", "/newsletter*", { status: 201 }).as("subscribe"); // intercept any POST HTTP request to localhost:3000/newsletter*
     cy.visit("/");
     cy.get('[data-cy="newsletter-email"]').type("test@example.com");
     cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait("@subscribe"); // this is not necessary, we use it just to make sure the next assumption executed after intercept
     cy.contains("Thanks for signing up!");
   });
 });
