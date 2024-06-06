b318f0d4ab6d347a07edb9f2f45d4944405135f5
Hadjati Darius <darius.hadjati@gmail.com>
2024-06-06 19:40:16 +0200

    More Intercepting!

--- 06-network-auth-starting-project/cypress/e2e/newsletter.cy.js
+++ 06-network-auth-starting-project/cypress/e2e/newsletter.cy.js
@@ -10,4 +10,15 @@ describe("Newsletter", () => {
     cy.wait("@subscribe"); // this is not necessary, we use it just to make sure the next assumption executed after intercept
     cy.contains("Thanks for signing up!");
   });
+
+  it("should display validation error message", () => {
+    cy.intercept("POST", "/newsletter*", {
+      message: "Email exists already",
+    }).as("subscribe"); // intercept any POST HTTP request to localhost:3000/newsletter*
+    cy.visit("/");
+    cy.get('[data-cy="newsletter-email"]').type("test@example.com");
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait("@subscribe"); // this is not necessary, we use it just to make sure the next assumption executed after intercept
+    cy.contains("Email exists already");
+  });
 });
