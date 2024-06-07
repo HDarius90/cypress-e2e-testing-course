8e5f87c95e0682843fe6b6ead05fbd8a9d27c541
Hadjati Darius <darius.hadjati@gmail.com>
2024-06-07 16:08:35 +0200

    Accessing Interceptor Request & Response Data

--- 06-network-auth-starting-project/cypress/e2e/takeaways.cy.js
+++ 06-network-auth-starting-project/cypress/e2e/takeaways.cy.js
@@ -8,4 +8,17 @@ describe("Takeaways", () => {
     cy.visit("/");
     cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
   });
+
+  it("should add a new takeaways", () => {
+    cy.intercept("POST", "/takeaways/new*", "success").as("newTakeaway");
+    cy.login();
+    cy.visit("/takeaways/new");
+    cy.get('[data-cy="title"]').click();
+    cy.get('[data-cy="title"]').type("TestTitle1");
+    cy.get('[data-cy="body"]').type("TestBody1");
+    cy.get('[data-cy="create-takeaway"]').click();
+    cy.wait("@newTakeaway")
+      .its("request.body")
+      .should("match", /TestTitle1.*TestBody1/);
+  });
 });
